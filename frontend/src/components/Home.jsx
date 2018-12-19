22b56180002adb6e06a8a66769847c4c0be5a1f6
Unknown <akhil@zuru.tech>
2018-12-19 18:25:51 +0530

    feat: Added Apollo, GraphQL, Bootstrap Packages

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
 
 export default class Home extends Component {
   render() {
-    return <div>Its Live</div>;
+    return (
+      <div>
+        <Button bsStyle="success">Success</Button>
+      </div>
+    );
   }
 }
