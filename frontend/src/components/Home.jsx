9592e5d4f5dd87a6cd9c6c1337c92c611d08d81a
Unknown <akhil@zuru.tech>
2018-12-19 19:03:19 +0530

    feat: Added folder structure of components

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,7 +5,7 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <Button bsStyle="success">Success</Button>
+        <Button bsStyle="success" />
       </div>
     );
   }
