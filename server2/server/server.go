58cc8e6971cdae43d0097caf9b9bc731de3d42c3
Aneri Shah <anerishah36@gmail.com>
2018-12-20 16:13:05 +0530

    add : websocket
    add websocket to server

--- server2/server/server.go
+++ server2/server/server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/aneri/chat-go-graphql/server2"
 	server "github.com/aneri/chat-go-graphql/server2"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
 const defaultPort = "3000"
@@ -21,8 +22,23 @@ func main() {
 	router := mux.NewRouter()
 	router.Use(server.MiddleWareHandler)
 	router.Handle("/", handler.Playground("GraphQL playground", "/query"))
-	router.Handle("/query", handler.GraphQL(server2.NewExecutableSchema(server2.Config{Resolvers: &server2.Resolver{}})))
+	router.Handle("/query", corsAccess(handler.GraphQL(server2.NewExecutableSchema(server2.Config{Resolvers: &server2.Resolver{}}),
+		handler.WebsocketUpgrader(websocket.Upgrader{
+			CheckOrigin: func(request *http.Request) bool {
+				return true
+			},
+		}),
+	)))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
+}
+func corsAccess(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+		response.Header().Set("Access-Control-Allow-Origin", "*")
+		response.Header().Set("Access-Control-Allow-Credentials", "true")
+		response.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		response.Header().Set("Access-Control-Allow-Headers", "Accept, X-Requested-With, Content-Type, Authorization")
+		next(response, request)
+	})
 }
