4486fae54c41c60628c0969e73dde3930fc879bc
Unknown <akhil@zuru.tech>
2018-12-20 17:48:15 +0530

    feat: Added Apollo and packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import Home from "./components/Home";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "http://192.168.1.145:9000/query"
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Home />
+        </div>
+      </ApolloProvider>
     );
   }
 }
