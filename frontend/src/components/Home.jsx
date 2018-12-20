067546637b7a0e6ae7be5acfbd1ed6a73cc8e01d
Unknown <akhil@zuru.tech>
2018-12-20 13:02:02 +0530

    feat: Added Chatbox

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Row, Col } from "react-bootstrap";
 import SelfName from "./SelfName/SelfName";
-import ChatBot from "./ChatBox/ChatBot";
+import ChatBox from "./ChatBox/ChatBox";
 import UserList from "./UserList/UserList";
 
 export default class Home extends Component {
@@ -10,15 +10,15 @@ export default class Home extends Component {
       <div className="homepage">
         <Grid>
           <Row>
-            <Col xs={12} sm={5} md={5}>
+            <Col xs={12} sm={6} md={6}>
               <SelfName />
             </Col>
-            <Col xs={12} sm={7} md={7}>
+            <Col xs={12} sm={6} md={6}>
               <UserList />
             </Col>
           </Row>
 
-          <ChatBot />
+          <ChatBox />
         </Grid>
       </div>
     );
--- frontend/src/components/SelfName/SelfName.jsx
+++ frontend/src/components/SelfName/SelfName.jsx
@@ -1,14 +1,57 @@
 import React, { Component } from "react";
 
 export default class SelfName extends Component {
+  constructor() {
+    super();
+    this.state = {
+      enterNameDisplay: "",
+
+      welcomeName: "",
+      welcomeDisplay: "none"
+    };
+
+    this.nameEntered = this.nameEntered.bind(this);
+  }
+
+  nameEntered(e) {
+    if (e.key === "Enter") {
+      if (e.target.value === "") {
+        alert("Please Enter a Name");
+      } else {
+        this.setState({
+          welcomeName: e.target.value,
+          welcomeDisplay: "",
+          enterNameDisplay: "none"
+        });
+      }
+    }
+  }
+
   render() {
     return (
-      <div className="enter-name">
-        <input className="cbox" type="checkbox" />
-        <label className="add" htmlFor="cbox">
-          Enter Your Name
-        </label>
-        <input className="message" type="text" />
+      <div className="selfname">
+        <div
+          className="enter-name"
+          style={{ display: this.state.enterNameDisplay }}
+        >
+          <input className="cbox" type="checkbox" />
+          <label className="add" htmlFor="cbox">
+            Enter Your Name
+          </label>
+          <input
+            className="message"
+            type="text"
+            onKeyPress={this.nameEntered}
+          />
+        </div>
+        <div
+          className="welcome"
+          style={{
+            display: this.state.welcomeDisplay
+          }}
+        >
+          Welcome, {this.state.welcomeName}
+        </div>
       </div>
     );
   }
