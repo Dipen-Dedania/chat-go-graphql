215a9b14f0bb4228046c82407ed8c84e8b5f7401
Unknown <akhil@zuru.tech>
2018-12-21 19:21:30 +0530

    feat: This is the most confusing code

--- frontend/src/components/UserList/UserList.jsx
+++ frontend/src/components/UserList/UserList.jsx
@@ -1,23 +1,51 @@
 import React, { Component } from "react";
-import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 
 import SingleUser from "./SingleUser";
 
-export default class UserList extends Component {
+class UserList extends Component {
   constructor() {
     super();
     this.state = {
-      userListDisplay: "none"
+      userListDisplay: "none",
+      userSelected: ""
     };
 
     this.toggleUserList = this.toggleUserList.bind(this);
+    this.callBack = this.callBack.bind(this);
   }
 
   toggleUserList() {
     var userListDisplay = this.state.userListDisplay === "none" ? "" : "none";
     this.setState({
       userListDisplay: userListDisplay
+    });
+  }
+
+  callBack(namesss) {
+    this.setState(
+      {
+        userSelected: namesss
+      },
+      function() {
+        console.log(this.state.userSelected);
+      }
+    );
+  }
+
+  renderUserList() {
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
+    }
+    return this.props.data.users.map(user => {
+      return (
+        <SingleUser
+          key={user.id}
+          username={user.name}
+          callBackProp={this.callBack}
+        />
+      );
     });
   }
 
@@ -34,28 +62,19 @@ export default class UserList extends Component {
           className="ui middle aligned divided list"
           style={{ display: this.state.userListDisplay }}
         >
-          {/* QUERY */}
-          <Query
-            query={gql`
-              {
-                users {
-                  id
-                  name
-                }
-              }
-            `}
-          >
-            {({ loading, error, data }) => {
-              if (loading) return <p>Loading...</p>;
-              if (error) return <p>Error :(</p>;
-
-              return data.users.map(({ id, name }) => (
-                <SingleUser key={id} username={name} />
-              ));
-            }}
-          </Query>
+          {this.renderUserList()}
         </div>
       </div>
     );
   }
 }
+
+const query = gql`
+  {
+    users {
+      name
+    }
+  }
+`;
+
+export default graphql(query)(UserList);
